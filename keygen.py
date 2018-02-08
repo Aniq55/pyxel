bdaee82f1cc180bfc7424f7f2e638d924f6465af
Aniq55 <aniqrah@gmail.com>
2018-02-08 19:40:04 +0530

    Divided binary stream

--- keygen.py
+++ keygen.py
@@ -1,21 +1,26 @@
 import hashlib
 import getpass
 import binascii
-
+import math
 
 def max_val(ht, wdth):
     return max(ht, wdth)
 
 def keygen(psswd,H,W):
-    # Encode the string into a byte array
-    psswd_encoded = psswd.encode('utf-8')
-    # Generate hash value
-def p2b(text):
-	word = ['{}'.format(bin(int(hashlib.sha256(text).hexdigest(), 16)))]
-	print(word)           
-    
-    
-p2b("goodboy")
-    
+    word = ['{}'.format(bin(int(hashlib.sha256(psswd.encode('utf-8')).hexdigest(), 16)))]
+    print(word)
+    h_bit= int(math.ceil(math.log2(H)))
+    w_bit= int(math.ceil(math.log2(W)))
+    B= h_bit + w_bit
+    print(B, h_bit, w_bit)
+    bin_hash= str(word)[4:-2]
+    chunks = [(bin_hash[i:i+B]) for i in range(0,len(bin_hash),B)]
+    print(chunks)
+    init_tuple = [c for c in chunks if len(c)==B]
+    # Either XOR here
+    print(init_tuple)
+    init_tuple = [(c[0:w_bit],c[h_bit:w_bit+h_bit]) for c in init_tuple]
+    # or XOR here
+    print(init_tuple)
 
-    
+keygen("goodboy", 10, 22)
