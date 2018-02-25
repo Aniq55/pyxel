662d9157ebeab184dbbb7a753ef88212d136b926
Aniq55 <aniqrah@gmail.com>
2018-02-25 23:39:40 +0530

    Corrected XNOR function

--- keygen.py
+++ keygen.py
@@ -22,31 +22,34 @@ def key_init(psswd,H,W):
     init_tuple = [ ( int(c[0:w_bit][2:],2),int(c[h_bit:w_bit+h_bit][2:],2) ) for c in init_tuple]
     return init_tuple
 
-
-# Function to XNOR two numbers excluding the MSB
 def xnor(a,b):
-    # take a and b as integers, convert to binary string, perform xnor then return an integer
-    a= str(bin(a))
-    b= str(bin(b))
-    # print(a)
-    # print(b)
-    c= ['0','b']
-    if len(a) < len(b):
-        a,b = b,a
-    b= b[0:2]+'0'*(len(a)-len(b))+b[2:]
-    # print(a)
-    # print(b)
-    for i in range(2,len(b)):
-        a_char = a[i]
-        b_char = b[i]
-        if a_char=='1' and b_char=='1':
-            c.append('1')
-        elif a_char=='0' and b_char=='0':
-            c.append('1')
-        else:
-            c.append('0')
-    # print(''.join(c))
-    return int(''.join(c),2)
+    return abs(~(a^b))
+
+
+# Function to XNOR two numbers excluding the MSB: WRONG
+# def xnor_str(a,b):
+#     # take a and b as integers, convert to binary string, perform xnor then return an integer
+#     a= str(bin(a))
+#     b= str(bin(b))
+#     # print(a)
+#     # print(b)
+#     c= ['0','b']
+#     if len(a) < len(b):
+#         a,b = b,a
+#     b= b[0:2]+'0'*(len(a)-len(b))+b[2:]
+#     # print(a)
+#     # print(b)
+#     for i in range(2,len(b)):
+#         a_char = a[i]
+#         b_char = b[i]
+#         if a_char=='1' and b_char=='1':
+#             c.append('1')
+#         elif a_char=='0' and b_char=='0':
+#             c.append('1')
+#         else:
+#             c.append('0')
+#     # print(''.join(c))
+#     return int(''.join(c),2)
 
 
 # Function to generate the next sequence of tuples
